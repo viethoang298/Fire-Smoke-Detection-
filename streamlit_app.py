2a60f585ae8a7c368639e7efcb5f3fa2d4bb19a3
viethoang298 <95960532+viethoang298@users.noreply.github.com>
2023-12-22 22:40:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import yolov7
 import tempfile
 #sys.path.append("yolov7")
 
-model_yolov8 = "models/yolov8/weights/fire_model.pt"
+model_yolov8 = "YOLOv8"    #"models/yolov8/weights/fire_model.pt"
 model_yolov7 = "models/yolov7/runs/train/exp/weights/best.pt"
 
 st.set_page_config(
